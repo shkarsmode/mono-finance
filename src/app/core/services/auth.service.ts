73851a064c3a662456c5b1afcebed8a6c177062e
Daniil Shkarupa <d.shkarupa@boosteroid.com>
2024-02-16 17:31:14 +0200

    Add auth service

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { LocalStorage } from '@core/enums';
+import { IAccountInfo } from '@core/interfaces';
+import { BehaviorSubject, first } from 'rxjs';
+import { LocalStorageService } from './local-storage.service';
+import { MonobankService } from './monobank.service';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthService {
+    public readonly authData$: BehaviorSubject<
+        IAccountInfo | { error: string } | any
+    > = new BehaviorSubject(null);
+
+    constructor(
+        private readonly localStorageService: LocalStorageService,
+        private readonly monobankService: MonobankService
+    ) {}
+
+    public processToken(token: string): void {
+        // this.localStorageService.set(LocalStorage.MonobankToken, token);
+        localStorage.setItem(LocalStorage.MonobankToken, token);
+        this.monobankService
+            .getClientInfo()
+            .pipe(first())
+            .subscribe((res) => this.authData$.next(res));
+    }
+}
