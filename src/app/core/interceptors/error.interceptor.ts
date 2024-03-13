3d15d67de7209e3e6226c4e6252ca199fb7bc848
Daniil Shkarupa <d.shkarupa@boosteroid.com>
2024-03-13 14:16:55 +0200

    update interceptor

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -5,13 +5,17 @@ import {
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private readonly snackBar: MatSnackBar) {}
+    constructor(
+        private readonly router: Router,
+        private readonly snackBar: MatSnackBar,
+    ) {}
 
     public intercept(
         request: HttpRequest<any>,
@@ -19,6 +23,10 @@ export class ErrorInterceptor implements HttpInterceptor {
     ) {
         return next.handle(request).pipe(
             catchError((error: any) => {
+                if (error.error.statusCode === 401) {
+                    this.router.navigateByUrl('/login');
+                }
+
                 const params = this.removeBasePathUrl(error.url);
                 const { errorDescription } = error.error;
 
